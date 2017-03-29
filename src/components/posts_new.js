e776b006ed4079c5012f56f31dc4433df8669c34
fz elhammoumi <fz.elhammoumi@gmail.com>
2017-03-29 15:35:09 +0100

    add new staff

---
+++ src/components/posts_new.js
@@ -0,0 +1,12 @@
+import React, { Component }  from 'react';
+import { connect } from 'react-redux';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>Create New Post</div>
+    );
+  }
+}
+
+export default PostsNew;
